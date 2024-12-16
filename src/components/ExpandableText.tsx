0d298d0873d7ce4c487e4245f3510bcde0335abb
LakshX0 <lakshansandeep7@gmail.com>
2024-12-16 06:40:33 +0530

    added useState to the More/Less toggle button

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+interface Props {
+  children: string;
+  maxChar?: number;
+}
+
+const ExpandableText = ({ children, maxChar = 100 }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  if (children.length <= maxChar) return <p>{children}</p>;
+
+  const text = isExpanded ? children : children.substring(0, maxChar);
+  return (
+    <p>
+      {text}...{" "}
+      <button onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? "Less" : "More"}
+      </button>
+    </p>
+  );
+};
+
+export default ExpandableText;
